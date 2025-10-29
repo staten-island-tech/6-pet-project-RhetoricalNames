ecdd8939bbb304075bb8fa714dac6c93f53822b4
Vincent <vincentz92@nycstudents.net>
2025-10-29 10:17:12 -0400

    Update pet.py

--- pet.py
+++ pet.py
@@ -14,25 +14,42 @@ Adventurer = Hero("Cubical", 0, [])
 Adventurer.buy({"title": "Sword", "atk": 1})
 print(Adventurer.__dict__) """
 
-class Pet:
+#L2
+class User: #Parent class
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
+    def display_info(self):
+        print(f"User: {self.name}, Email: {self.email}")
+
+class Student(User): #Child class, calls parent class
+    def __init__(self, name, email, student_id):
+        super().__init__(name, email)  # Call the parent class constructor
+        self.student_id = student_id
+    
+    def display_info(self):
+        print(f"Student: {self.name}, Email: {self.email}, Student ID: {self.student_id}")
+studentthing = Student("csmajor", "csmajor@email.com", "1234567890")
+studentthing.display_info()
+""" class Pet:
     def __init__(self, name, happiness, hunger):
         self.name = name
-        self.happiness = happiness
-        self.hunger = hunger
+        self.__happiness = happiness
+        self.__hunger = hunger
 
     def play(self, time):
-        self.happiness += (random.randint(5,10) + time)
-        self.hunger += (random.randint(5,10) + time)/(2)
+        self.__happiness += (random.randint(5,10) + time)
+        self.__hunger += (random.randint(5,10) + time)/(2)
 
     def feed(self, food):
-        self.hunger -= (random.randint(10, 20) - food)
-        if self.hunger < 0:
-            self.hunger = 0
+        self.__hunger -= (random.randint(10, 20) - food)
+        if self.__hunger < 0:
+            self.__hunger = 0
 
     def status(self):
-        print(Pikachu.hunger)
-        print(Pikachu.happiness)
+        print(Pikachu.__happiness)
 Pikachu = Pet("Pikachu", 10, 0)
 Pikachu.play(5)
 Pikachu.feed(10)
-print(Pikachu.__dict__)
+Pikachu.status() """
